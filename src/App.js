d71cf9d7d13309f364801dd4ffa3630379d77d5b
Merin <mmerin@northwell.edu>
2018-03-08 21:33:48 -0500

    pw protect cipher

--- src/App.js
+++ src/App.js
@@ -50,31 +50,25 @@ export default class App extends Component {
       })
     }
   }
-// 
-//   var map = {
-//     a: 'q', b: 'w', c: 'e',
-//     d: 'r', e: 't', f: 'y',
-//     g: 'u', h: 'i', i: 'o',
-//     j: 'p', k: 'a', l: 's',
-//     m: 'd', n: 'f', o: 'g',
-//     p: 'h', q: 'j', r: 'k',
-//     s: 'l', t: 'z', u: 'x',
-//     v: 'c', w: 'v', x: 'b',
-//     y: 'n', z: 'm', 1:2, 2:3, 3:4, 4:5, 5:6, 6:7, 7:8, 8:9, 9:0, 0:1
-// };
-//
-// s.split('').filter(function(v) {
-//     // Does the character exist in the map?
-//     return map.hasOwnProperty(v.toLowerCase());
-// }).map(function(v) {
-//     // Replace character by value
-//     return map[v.toLowerCase()].toUpperCase();
-// }).join("");
 
   loginPassword = () => {
-    var pw = "Lenticular8844";
+    var pw = "Stfzoexsqk9955";
+    var map = {
+      a: 'q', b: 'w', c: 'e', d: 'r', e: 't', f: 'y',
+      g: 'u', h: 'i', i: 'o', j: 'p', k: 'a', l: 's',
+      m: 'd', n: 'f', o: 'g', p: 'h', q: 'j', r: 'k',
+      s: 'l', t: 'z', u: 'x', v: 'c', w: 'v', x: 'b',
+      y: 'n', z: 'm',
+      A: 'Q', B: 'W', C: 'E', D: 'R', E: 'T', F: 'Y',
+      G: 'U', H: 'I', I: 'O', J: 'P', K: 'A', L: 'S',
+      M: 'D', N: 'F', O: 'G', P: 'H', Q: 'J', R: 'K',
+      S: 'L', T: 'Z', U: 'X', V: 'C', W: 'V', X: 'B',
+      Y: 'N', Z: 'M',
+      1: '2', 2: '3', 3: '4', 4: '5', 5: '6',
+      6: '7', 7: '8', 8: '9', 9: '0', 0: '1'
+    };
     var text = prompt("Please enter the password to continue");
-    if (text !== pw) {
+    if (text.split("").map(x => map[x]).join("") !== pw) {
       alert("Sorry wrong password, please try again.")
       this.loginPassword();
     }
