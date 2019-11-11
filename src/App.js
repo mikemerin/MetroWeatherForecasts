3a93d87d006b05679ec691c969b39ac0581889b9
Mike Merin <mikemerinweather@gmail.com>
2019-11-10 22:42:47 -0500

    new UI

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
       current: 0,
       season: season,
       data: [ {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {} ],
-      debug: true
+      debug: false
     }
   }
 
