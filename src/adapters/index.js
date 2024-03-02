f653c23e1f9d0cc9c081d8d6fe54d50780db158d
Mike Merin <mike.d.merin@gmail.com>
2024-03-02 14:32:19 -0500

    Update index.js

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -4,8 +4,8 @@ import { Tempdata, Temptidedata } from './Tempdata'
 
 const shift = str => str.slice(1) + str.slice(0,1);
 
-const clientId = shift('kzaqg0xMGuT8KDODCLVMQ');
-const clientSecret = shift('F86RYAKPGy9IsBv04ruOjQaSYEXI8wQl0UNnlQzI');
+const clientId = shift('HKFXShcFnYvapC5p2Mw8Y');
+const clientSecret = shift('beErXU4jsGx14eshtQxLgrXJExcd1qjNofrIPjPs');
 const U = scramble('vrpmrspq');
 
 const id_secret = `?client_id=${clientId}&client_secret=${clientSecret}`;
